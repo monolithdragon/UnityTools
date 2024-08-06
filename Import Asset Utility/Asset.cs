14950b519d996a8f19f3140fb1b9e960baa4013f
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-08-06 15:49:53 +0200

    Create unity asset importer

---
+++ Import Asset Utility/Asset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UnityTools.ImportAssetUtility {
+    public class Asset {
+        /// <summary>
+        /// Name of asset;
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Asset folder.
+        /// </summary>
+        public string SubFolder { get; }
+
+        /// <summary>
+        /// Asset store folder.
+        /// </summary>
+        public string StorePath { get; }
+
+        /// <summary>
+        /// Initialize a new <see cref="Asset"/> instance.
+        /// </summary>
+        /// <param name="name">Name of asset</param>
+        /// <param name="subFolder">The asset folder</param>
+        public Asset(string assetName, string subFolder) {
+            Name = assetName;
+            SubFolder = subFolder;
+            StorePath =
+                $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/Unity/Asset Store-5.x";
+        }
+    }
+}
---
+++ Import Asset Utility/InportAssetEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityTools.ImportAssetUtility {
+    public class ImportAssetEditor : Editor {
+        [MenuItem("UnityTools/Import Essentials Asset")]
+        public static void Execute() {
+            ImportEssentials(new Asset("DOTween HOTween v2.unitypackage", "Demigiant/Editor ExtensionsAnimation"));
+        }
+
+        private static void ImportEssentials(Asset asset) {
+            Debug.Log("Importing: <b>" + asset.Name + "</b>");
+            AssetDatabase.ImportPackage(Path.Combine(asset.StorePath, asset.SubFolder, asset.Name), false);
+        }
+    }
+}
